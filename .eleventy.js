3ccb3303af96e6f9b5bfcbc0e1622dc71b6d333e
Atalanta <atalanta@atalanta.lan>
2021-08-04 17:04:30 -0300

    create first version

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
-
+  eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
